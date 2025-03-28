786efd5a68a3f41d4eb14b3e1a45f4bd66022e83
MJ <gwang907@gmail.com>
2025-03-28 13:15:58 +0900

    design: stationListPage 레이아웃 배치

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_train_seat_app/home_page.dart';
+import 'package:flutter_train_seat_app/station_list_page.dart';
 
 void main() {
   runApp(const MyApp());
@@ -11,5 +12,6 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(home: HomePage());
+    // return MaterialApp(home: StationListPage()); **레이아웃 확인용 코드
   }
 }
--- lib/station_list_page.dart
+++ lib/station_list_page.dart
@@ -3,6 +3,41 @@ import 'package:flutter/material.dart';
 class StationListPage extends StatelessWidget {
   @override
   Widget build(Object context) {
-    return Text('StationListPage');
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('출발역'),
+        leading: IconButton(
+          icon: const Icon(Icons.navigate_before),
+          onPressed: () {},
+        ),
+      ),
+      body: Column(
+        children: [
+          stationItem('수서'),
+          stationItem('동탄'),
+          stationItem('평택지제'),
+          stationItem('천안아산'),
+          stationItem('오송'),
+          stationItem('대전'),
+        ],
+      ),
+    );
+  }
+
+  Column stationItem(String string) {
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      children: [
+        SizedBox(height: 4),
+        Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 16),
+          child: Text(
+            string,
+            style: TextStyle(fontSize: 17, fontWeight: FontWeight.bold),
+          ),
+        ),
+        Divider(color: Colors.grey[700], thickness: 0.4),
+      ],
+    );
   }
 }
